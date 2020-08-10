bb757adb8f22d01933fc1d6555ac1a291601f570
liyihan <1638401515@qq.com>
2020-08-10 22:03:18 +0800

    add student

---
+++ src/main/java/com/fh/shop/admin/student/Student.java
@@ -0,0 +1,11 @@
+package com.fh.shop.admin.student;
+
+public class Student {
+
+    public static void main(String[] args){
+
+        System.out.println("阳阳吖");
+        System.out.println("日子照过傍晚的风 清凉的啤酒哪一样不是快乐");
+
+    }
+}
